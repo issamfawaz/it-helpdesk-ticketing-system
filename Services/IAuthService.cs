01068de9f3dd6eae42d3349c6113805278e05794
issamfawaz <issamfawazfawaz@gmail.com>
2026-06-08 22:20:05 +0300

    Delete Services directory

--- Services/IAuthService.cs
+++
@@ -1,9 +0,0 @@
-using Helpdesk.Api.Models;
-
-namespace Helpdesk.Api.Services;
-
-public interface IAuthService
-{
-    AppUser? ValidateUser(string email, string password);
-}
-
--- Services/IJwtTokenService.cs
+++
@@ -1,9 +0,0 @@
-using Helpdesk.Api.Models;
-
-namespace Helpdesk.Api.Services;
-
-public interface IJwtTokenService
-{
-    LoginResponse CreateToken(AppUser user);
-}
-
